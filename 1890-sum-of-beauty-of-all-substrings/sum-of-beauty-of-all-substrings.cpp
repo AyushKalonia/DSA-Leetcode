04f86931bf8e916ae212b70fb41a89c4ee0efafc
Ayush Kalonia <ayushkalonia007@gmail.com>
2026-06-05 21:00:34 +0530

    Time: 51 ms (93.88%) | Memory: 11.2 MB (65.46%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int n = s.length();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                int maxi = 0;
+                int mini = INT_MAX;
+
+                for (int x : freq) {
+                    if (x > 0) {
+                        maxi = max(maxi, x);
+                        mini = min(mini, x);
+                    }
+                }
+
+                sum += maxi - mini;
+            }
+        }
+
+        return sum;
+    }
+};
