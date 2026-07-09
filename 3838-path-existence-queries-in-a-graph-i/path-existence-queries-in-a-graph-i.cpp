8579a11fe23b5bc581a3156c7b439850c2d41016
Ayush Kalonia <ayushkalonia007@gmail.com>
2026-07-09 21:19:36 +0530

    Time: 11 ms (73.15%) | Memory: 224.2 MB (66.03%) - LeetSync

---
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<int> component(n);
+
+        for (int i = 1; i < n; i++) {
+            component[i] = component[i - 1];
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                component[i]++;
+            }
+        }
+
+        vector<bool> ans;
+
+        for (auto& query : queries) {
+            int u = query[0];
+            int v = query[1];
+
+            ans.push_back(component[u] == component[v]);
+        }
+
+        return ans;
+    }
+};
