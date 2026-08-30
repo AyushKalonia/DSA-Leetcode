f3a5f0db77d89e8aa2b86831d5df8dec6bd63746
Ayush Kalonia <ayushkalonia007@gmail.com>
2026-08-30 21:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp){
-        if(n<=2)    return n;
-        if(dp[n]!=-1)   return dp[n];
-
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-
-        return solve(n, dp);
+       int arr[n+1];
+       if(n==0||n==1||n==2)return n;
+       arr[0]=0;
+       arr[1]=1;
+       arr[2]=2;
+       for(int i=3;i<=n;i++)
+       {
+        arr[i]=arr[i-1]+arr[i-2];
+       }
+       return arr[n];
     }
+
 };
